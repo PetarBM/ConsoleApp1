aa2100f6609689f474043d666ae4f6b0fa85b9ab
PetarBM <Petar.balaic@PETAR>
2026-03-26 18:16:11 +0100

    Drugi commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
+namespace ConsoleApp1 
+{ 
+    class Program 
+    {     static void Main(string[] args) 
+        { 
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Novo");
+        }
+    }
+}
